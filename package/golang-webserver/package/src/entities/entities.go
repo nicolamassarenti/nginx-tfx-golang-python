a3247b792d3a6cd33c5c3d50e19face22f8af7b1
nicolamassarenti <nicola.massarenti@gmail.com>
2021-05-30 17:56:28 +0200

    Modified entities

--- package/golang-webserver/package/src/entities/entities.go
+++ package/golang-webserver/package/src/entities/entities.go
@@ -2,12 +2,29 @@ package entities
 
 type Sentence struct {
 	Text      string
-	Sentiment string
+	NLP NLPAnalysis
 	Stats     SentenceStats
+}
+
+type NLPAnalysis struct {
+	Sentiment float32
+	Embedding []float32
+	Tokens    []Token
+}
+
+type Token struct {
+	Lemma string
+	POS 	string
 }
 
 type SentenceStats struct {
 	NumWords   int
 	Length     int
 	IsQuestion int
+}
+
+type SentenceAnalyzer interface {
+	Sentiment(sentence string) (sentiment float32, err error)
+	Tokens(sentence string) (tokens []Token, err error)
+	Embedding(sentence string) (embedding []float32, err error)
 }
